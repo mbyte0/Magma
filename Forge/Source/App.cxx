82efbaba5f6345655f9d9029a56cc7f568f1c76d
mbyte0 <169203892+mbyte0@users.noreply.github.com>
2024-10-11 13:53:06 +0200

    Remove files for build test

--- Forge/Source/App.cxx
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "App.hxx"
-
-App::App(std::string_view name)
-: m_Name(name) {}
-
-void App::Print(std::string_view sv) {
-  std::cout << "[" << m_Name << "] " << sv << std::endl;
-}
--- Forge/Source/Main.cxx
+++
@@ -1,7 +0,0 @@
-#include "Lib.hxx"
-#include "App.hxx"
-
-int main() {
-  App app("app name");
-  app.Print(Lib::GetVersion());
-}
